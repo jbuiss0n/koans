3c29927a8708128368e99da5fabcb74e04c6a8e9
jbuissOn <jeremy.buisson@live.com>
2018-11-13 16:27:42 +0100

    feat(triangle-illegal)

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError.new if a <=0 || b <= 0 || c <= 0
+  raise TriangleError.new if a + b <= c || a + c <= b || b + c <= a
+
   return :equilateral if a == b && b == c
   return :isosceles if a == b || a == c || b == c
   return :scalene
